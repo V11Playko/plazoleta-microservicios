af3e4d0597c4413c614ebfb8d3bd480888a2a905
Heinner Vega <heinnervega20@gmail.com>
2023-02-15 18:49:58 -0500

    first commit

---
+++ src/main/java/com/pragma/powerup/PlazaDeComidas.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaDeComidas {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazaDeComidas.class, args);
+	}
+
+}
